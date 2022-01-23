6b02c4a8616e54a2c2e27e85c9de86188b96bb41
Flix <f.dommes@famedly.com>
2022-01-23 21:17:06 +0100

    fix: make classifier Send+Sync

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::{env, sync::Arc};
+use std::{
+	env,
+	sync::{Arc, Mutex},
+};
 
 use color_eyre::Result;
 use futures::future::try_join_all;
 use rust_bert::pipelines::sentiment::{SentimentConfig, SentimentModel};
 use sqlx::PgPool;
-use tokio::{
-	runtime::Runtime,
-	task::{self, LocalSet},
-};
+use tokio::{runtime::Runtime, task};
 use twitter_sentiment::*;
 
 // TODO:
@@ -29,7 +29,8 @@ fn main() -> Result<()> {
 	dotenv::dotenv()?;
 
 	// must be created outside of the async runtime :(
-	let sentiment_classifier = SentimentModel::new(SentimentConfig::default())?;
+	let sentiment_classifier =
+		Arc::new(Mutex::new(SentimentModel::new(SentimentConfig::default())?));
 
 	Runtime::new()?.block_on(async {
 		// Init DB
@@ -43,7 +44,7 @@ fn main() -> Result<()> {
 		let twitter_streams = TwitterStreamRunner::builder()
 			.streams(config.track_tweets.as_slice())
 			.token(token)
-			.sentiment_classifier(Arc::new(sentiment_classifier))
+			.sentiment_classifier(sentiment_classifier)
 			.db(db.clone())
 			.build()?;
 
@@ -52,8 +53,7 @@ fn main() -> Result<()> {
 			Server::builder().bind(server_addr).db(db.clone()).config(Arc::new(config)).build()?;
 
 		// Run all tasks/jobs/runners
-		let handles = vec![task::spawn(server.run())];
-		LocalSet::new().run_until(twitter_streams.run()).await?;
+		let handles = vec![task::spawn(twitter_streams.run()), task::spawn(server.run())];
 		for res in try_join_all(handles).await? {
 			res?;
 		}
--- src/twitter_stream.rs
+++ src/twitter_stream.rs
@@ -1,12 +1,13 @@
 //! Runner to receive the twitter streams and put sentiment data into the DB
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use color_eyre::Result;
 use derive_builder::Builder;
 use egg_mode::{search::ResultType, stream::StreamMessage, Token};
 use futures::StreamExt;
 use rust_bert::pipelines::sentiment::{Sentiment, SentimentModel, SentimentPolarity};
+use tokio::task;
 
 use crate::database::{self, SentimentDB};
 
@@ -23,13 +24,22 @@ pub struct TwitterStreamRunner {
 	#[builder(setter(into))]
 	streams: Vec<String>,
 	token: Token,
-	sentiment_classifier: Arc<SentimentModel>,
+	// Needs mutex to be Send + Sync
+	sentiment_classifier: Arc<Mutex<SentimentModel>>,
 	db: Arc<SentimentDB>,
 }
 
 impl TwitterStreamRunner {
 	pub fn builder() -> TwitterStreamRunnerBuilder {
 		TwitterStreamRunnerBuilder::default()
+	}
+
+	/// Predict sentiment of some texts
+	fn predict_sentiment<'a>(&self, texts: &[&'a str]) -> Vec<Sentiment> {
+		task::block_in_place(|| {
+			let classifier = self.sentiment_classifier.lock().expect("access sentiment classifier");
+			classifier.predict(texts)
+		})
 	}
 
 	/// If the database for a keyword is empty, fill it with some search results
@@ -52,7 +62,7 @@ impl TwitterStreamRunner {
 			for tweet in tweets.response {
 				let id = tweet.id;
 				let created = tweet.created_at.timestamp();
-				let sentiment = self.sentiment_classifier.predict(&[tweet.text.as_str()]);
+				let sentiment = self.predict_sentiment(&[tweet.text.as_str()]);
 
 				let entry = database::TweetSentiment::new(
 					id,
@@ -83,7 +93,7 @@ impl TwitterStreamRunner {
 				let text = tweet.text;
 
 				for keyword in self.streams.iter().filter(|key| text.contains(*key)) {
-					let sentiment = self.sentiment_classifier.predict(&[text.as_str()]);
+					let sentiment = self.predict_sentiment(&[text.as_str()]);
 
 					let entry = database::TweetSentiment::new(
 						id,
