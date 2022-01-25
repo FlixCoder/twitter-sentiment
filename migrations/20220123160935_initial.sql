3c7e38e9d073b6c83f3e5b6893e8cef61d9f4436
Flix <f.dommes@famedly.com>
2022-01-25 09:06:32 +0100

    fix: Runtime exection problems

--- migrations/20220123160935_initial.sql
+++ migrations/20220123160935_initial.sql
@@ -1,6 +1,7 @@
 CREATE TABLE tweet_sentiment (
-	id BIGINT PRIMARY KEY NOT NULL,
+	id BIGINT NOT NULL,
 	keyword VARCHAR(101) NOT NULL,
+	UNIQUE (id, keyword),
 	created BIGINT NOT NULL,
 	sentiment DOUBLE PRECISION NOT NULL
 );
